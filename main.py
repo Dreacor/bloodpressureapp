1504b53c7d0d3a9b16588ed1d549cab6fc0f9aa1
Dreacor <juho.lang@gmail.com>
2026-04-01 22:13:04 +0300

    Actual code added for checking my bloodpressure

---
+++ main.py
@@ -0,0 +1,43 @@
+# Verenpaineen mittauksen keskiarvot 
+
+def main():
+    yla_aamu = [121, 119, 124, 117, 118]
+    yla_ilta = [135, 125, 112, 125]
+    ala_aamu = [92, 71, 80, 82, 75]
+    ala_ilta = [79, 78, 76, 85]
+
+    yla_paine_aamu = 0
+    yla_paine_ilta = 0
+    ala_paine_aamu = 0
+    ala_paine_ilta = 0
+    
+    for paine in yla_aamu:
+        yla_paine_aamu += paine
+    
+    for paine in yla_ilta:
+        yla_paine_ilta += paine
+    
+    for paine in ala_aamu:
+        ala_paine_aamu += paine
+    
+    for paine in ala_ilta:
+        ala_paine_ilta += paine
+
+    avg_yla_aamu = yla_paine_aamu / len(yla_aamu)
+    avg_yla_ilta = yla_paine_ilta / len(yla_ilta)
+    avg_ala_aamu = ala_paine_aamu / len(ala_aamu)
+    avg_ala_ilta = ala_paine_ilta / len(ala_ilta)
+    avg_yla = (yla_paine_aamu + yla_paine_ilta) / (len(yla_aamu) + len(yla_ilta))
+    avg_ala = (ala_paine_aamu + ala_paine_ilta) / (len(ala_aamu) + len(ala_ilta))
+
+    print(f"Keskiverto yläpaine aamulla: {round(avg_yla_aamu)}")
+    print(f"Keskiverto yläpaine illalla: {round(avg_yla_ilta)}")
+    print(f"Keskiverto alapaine aamulla: {round(avg_ala_aamu)}")
+    print(f"Keskiverto alapaine illalla: {round(avg_ala_ilta)}")
+
+    print(f"Keskiverto yläpaine: {round(avg_yla)}")
+    print(f"keskiverto alapaine: {round(avg_ala)}")
+
+main()
+
+
